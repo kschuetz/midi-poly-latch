dbf2cb70670ff436dd246d06debfd0dc13891334
Kevin Schuetz <schuetzk@gmail.com>
2022-07-26 23:24:25 -0500

    Use CHECK instead of REQUIRE

--- tests/NoteIndexTests.cpp
+++ tests/NoteIndexTests.cpp
@@ -1,68 +1,68 @@
 #include <catch2/catch_all.hpp>
 #include "core/NoteIndex.h"
 
-#define TEST_GROUP "[NoteIndex]"
+#define NOTE_INDEX_TAG "[NoteIndex]"
 
-TEST_CASE("inserts notes in the proper position", TEST_GROUP) {
+TEST_CASE("inserts notes in the proper position", NOTE_INDEX_TAG) {
     NoteIndex ni;
-    REQUIRE(ni.insert(0) == 0);
-    REQUIRE(ni.positionOf(0) == 0);
-    REQUIRE(ni.contains(0));
-    REQUIRE(ni.size() == 1);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 0);
+    CHECK(ni.insert(0) == 0);
+    CHECK(ni.positionOf(0) == 0);
+    CHECK(ni.contains(0));
+    CHECK(ni.size() == 1);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 0);
 
-    REQUIRE(ni.insert(1) == 1);
-    REQUIRE(ni.positionOf(1) == 1);
-    REQUIRE(ni.contains(1));
-    REQUIRE(ni.size() == 2);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 1);
+    CHECK(ni.insert(1) == 1);
+    CHECK(ni.positionOf(1) == 1);
+    CHECK(ni.contains(1));
+    CHECK(ni.size() == 2);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 1);
 
-    REQUIRE(ni.insert(0) == 0);
-    REQUIRE(ni.positionOf(0) == 0);
-    REQUIRE(ni.contains(0));
-    REQUIRE(ni.size() == 2);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 1);
+    CHECK(ni.insert(0) == 0);
+    CHECK(ni.positionOf(0) == 0);
+    CHECK(ni.contains(0));
+    CHECK(ni.size() == 2);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 1);
 
-    REQUIRE(ni.insert(127) == 2);
-    REQUIRE(ni.positionOf(127) == 2);
-    REQUIRE(ni.contains(127));
-    REQUIRE(ni.size() == 3);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 127);
+    CHECK(ni.insert(127) == 2);
+    CHECK(ni.positionOf(127) == 2);
+    CHECK(ni.contains(127));
+    CHECK(ni.size() == 3);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 127);
 
-    REQUIRE(ni.insert(64) == 2);
-    REQUIRE(ni.positionOf(64) == 2);
-    REQUIRE(ni.contains(64));
-    REQUIRE(ni.size() == 4);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 127);
+    CHECK(ni.insert(64) == 2);
+    CHECK(ni.positionOf(64) == 2);
+    CHECK(ni.contains(64));
+    CHECK(ni.size() == 4);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 127);
 
-    REQUIRE(ni.insert(65) == 3);
-    REQUIRE(ni.positionOf(65) == 3);
-    REQUIRE(ni.contains(65));
-    REQUIRE(ni.size() == 5);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 127);
+    CHECK(ni.insert(65) == 3);
+    CHECK(ni.positionOf(65) == 3);
+    CHECK(ni.contains(65));
+    CHECK(ni.size() == 5);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 127);
 
-    REQUIRE(ni.insert(96) == 4);
-    REQUIRE(ni.positionOf(96) == 4);
-    REQUIRE(ni.contains(96));
-    REQUIRE(ni.size() == 6);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 127);
+    CHECK(ni.insert(96) == 4);
+    CHECK(ni.positionOf(96) == 4);
+    CHECK(ni.contains(96));
+    CHECK(ni.size() == 6);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 127);
 
-    REQUIRE(ni.insert(2) == 2);
-    REQUIRE(ni.positionOf(2) == 2);
-    REQUIRE(ni.contains(2));
-    REQUIRE(ni.size() == 7);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 127);
+    CHECK(ni.insert(2) == 2);
+    CHECK(ni.positionOf(2) == 2);
+    CHECK(ni.contains(2));
+    CHECK(ni.size() == 7);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 127);
 }
 
-TEST_CASE("iterates correctly", TEST_GROUP) {
+TEST_CASE("iterates correctly", NOTE_INDEX_TAG) {
     NoteIndex ni;
     ni.insert(0);
     ni.insert(1);
@@ -74,54 +74,54 @@ TEST_CASE("iterates correctly", TEST_GROUP) {
 
     SECTION("forward") {
         auto it = ni.cbegin();
-        REQUIRE(*it == 0);
-        REQUIRE(*(++it) == 1);
-        REQUIRE(*(++it) == 2);
-        REQUIRE(*(++it) == 64);
-        REQUIRE(*(++it) == 65);
-        REQUIRE(*(++it) == 96);
-        REQUIRE(*(++it) == 127);
-        REQUIRE(++it == ni.cend());
+        CHECK(*it == 0);
+        CHECK(*(++it) == 1);
+        CHECK(*(++it) == 2);
+        CHECK(*(++it) == 64);
+        CHECK(*(++it) == 65);
+        CHECK(*(++it) == 96);
+        CHECK(*(++it) == 127);
+        CHECK(++it == ni.cend());
     }
 
     SECTION("reverse") {
         auto it = ni.crbegin();
-        REQUIRE(*it == 127);
-        REQUIRE(*(++it) == 96);
-        REQUIRE(*(++it) == 65);
-        REQUIRE(*(++it) == 64);
-        REQUIRE(*(++it) == 2);
-        REQUIRE(*(++it) == 1);
-        REQUIRE(*(++it) == 0);
-        REQUIRE(++it == ni.crend());
+        CHECK(*it == 127);
+        CHECK(*(++it) == 96);
+        CHECK(*(++it) == 65);
+        CHECK(*(++it) == 64);
+        CHECK(*(++it) == 2);
+        CHECK(*(++it) == 1);
+        CHECK(*(++it) == 0);
+        CHECK(++it == ni.crend());
     }
 }
 
-TEST_CASE("empty", TEST_GROUP) {
+TEST_CASE("empty", NOTE_INDEX_TAG) {
     NoteIndex ni;
 
-    REQUIRE(ni.size() == 0);
-    REQUIRE(ni.front() == NO_NOTE);
-    REQUIRE(ni.back() == NO_NOTE);
-    REQUIRE(ni.cbegin() == ni.cend());
-    REQUIRE(ni.crbegin() == ni.crend());
+    CHECK(ni.size() == 0);
+    CHECK(ni.front() == NO_NOTE);
+    CHECK(ni.back() == NO_NOTE);
+    CHECK(ni.cbegin() == ni.cend());
+    CHECK(ni.crbegin() == ni.crend());
 
     SECTION("contains no notes") {
         int containsCount = 0;
         for (NoteNumber n = 0; isValidNote(n); n++) {
             if (ni.contains(n)) containsCount++;
         }
-        REQUIRE(containsCount == 0);
+        CHECK(containsCount == 0);
     }
 }
 
-TEST_CASE("insert does not accept illegal notes", TEST_GROUP) {
+TEST_CASE("insert does not accept illegal notes", NOTE_INDEX_TAG) {
     NoteIndex ni;
-    REQUIRE(ni.insert(NO_NOTE) == -1);
-    REQUIRE(ni.insert(NO_NOTE - 1) == -1);
+    CHECK(ni.insert(NO_NOTE) == -1);
+    CHECK(ni.insert(NO_NOTE - 1) == -1);
 }
 
-TEST_CASE("removes notes", TEST_GROUP) {
+TEST_CASE("removes notes", NOTE_INDEX_TAG) {
     NoteIndex ni;
 
     ni.insert(0);
@@ -133,178 +133,178 @@ TEST_CASE("removes notes", TEST_GROUP) {
     ni.insert(127);
 
     SECTION("remove non-existing note") {
-        REQUIRE(ni.remove(126) == false);
-        REQUIRE(ni.size() == 7);
+        CHECK(ni.remove(126) == false);
+        CHECK(ni.size() == 7);
 
-        REQUIRE(ni.front() == 0);
-        REQUIRE(ni.back() == 127);
+        CHECK(ni.front() == 0);
+        CHECK(ni.back() == 127);
 
         auto it1 = ni.cbegin();
-        REQUIRE(*it1 == 0);
-        REQUIRE(*(++it1) == 1);
-        REQUIRE(*(++it1) == 2);
-        REQUIRE(*(++it1) == 64);
-        REQUIRE(*(++it1) == 65);
-        REQUIRE(*(++it1) == 96);
-        REQUIRE(*(++it1) == 127);
-        REQUIRE(++it1 == ni.cend());
+        CHECK(*it1 == 0);
+        CHECK(*(++it1) == 1);
+        CHECK(*(++it1) == 2);
+        CHECK(*(++it1) == 64);
+        CHECK(*(++it1) == 65);
+        CHECK(*(++it1) == 96);
+        CHECK(*(++it1) == 127);
+        CHECK(++it1 == ni.cend());
 
         auto it2 = ni.crbegin();
-        REQUIRE(*it2 == 127);
-        REQUIRE(*(++it2) == 96);
-        REQUIRE(*(++it2) == 65);
-        REQUIRE(*(++it2) == 64);
-        REQUIRE(*(++it2) == 2);
-        REQUIRE(*(++it2) == 1);
-        REQUIRE(*(++it2) == 0);
-        REQUIRE(++it2 == ni.crend());
+        CHECK(*it2 == 127);
+        CHECK(*(++it2) == 96);
+        CHECK(*(++it2) == 65);
+        CHECK(*(++it2) == 64);
+        CHECK(*(++it2) == 2);
+        CHECK(*(++it2) == 1);
+        CHECK(*(++it2) == 0);
+        CHECK(++it2 == ni.crend());
     }
 
     SECTION("remove existing note from middle") {
-        REQUIRE(ni.remove(64) == true);
-        REQUIRE(ni.size() == 6);
+        CHECK(ni.remove(64) == true);
+        CHECK(ni.size() == 6);
 
-        REQUIRE(ni.front() == 0);
-        REQUIRE(ni.back() == 127);
+        CHECK(ni.front() == 0);
+        CHECK(ni.back() == 127);
 
         auto it1 = ni.cbegin();
-        REQUIRE(*it1 == 0);
-        REQUIRE(*(++it1) == 1);
-        REQUIRE(*(++it1) == 2);
-        REQUIRE(*(++it1) == 65);
-        REQUIRE(*(++it1) == 96);
-        REQUIRE(*(++it1) == 127);
-        REQUIRE(++it1 == ni.cend());
+        CHECK(*it1 == 0);
+        CHECK(*(++it1) == 1);
+        CHECK(*(++it1) == 2);
+        CHECK(*(++it1) == 65);
+        CHECK(*(++it1) == 96);
+        CHECK(*(++it1) == 127);
+        CHECK(++it1 == ni.cend());
 
         auto it2 = ni.crbegin();
-        REQUIRE(*it2 == 127);
-        REQUIRE(*(++it2) == 96);
-        REQUIRE(*(++it2) == 65);
-        REQUIRE(*(++it2) == 2);
-        REQUIRE(*(++it2) == 1);
-        REQUIRE(*(++it2) == 0);
-        REQUIRE(++it2 == ni.crend());
+        CHECK(*it2 == 127);
+        CHECK(*(++it2) == 96);
+        CHECK(*(++it2) == 65);
+        CHECK(*(++it2) == 2);
+        CHECK(*(++it2) == 1);
+        CHECK(*(++it2) == 0);
+        CHECK(++it2 == ni.crend());
     }
 
     SECTION("remove existing note from front") {
-        REQUIRE(ni.remove(0) == true);
-        REQUIRE(ni.size() == 6);
+        CHECK(ni.remove(0) == true);
+        CHECK(ni.size() == 6);
 
-        REQUIRE(ni.front() == 1);
-        REQUIRE(ni.back() == 127);
+        CHECK(ni.front() == 1);
+        CHECK(ni.back() == 127);
 
         auto it1 = ni.cbegin();
-        REQUIRE(*it1 == 1);
-        REQUIRE(*(++it1) == 2);
-        REQUIRE(*(++it1) == 64);
-        REQUIRE(*(++it1) == 65);
-        REQUIRE(*(++it1) == 96);
-        REQUIRE(*(++it1) == 127);
-        REQUIRE(++it1 == ni.cend());
+        CHECK(*it1 == 1);
+        CHECK(*(++it1) == 2);
+        CHECK(*(++it1) == 64);
+        CHECK(*(++it1) == 65);
+        CHECK(*(++it1) == 96);
+        CHECK(*(++it1) == 127);
+        CHECK(++it1 == ni.cend());
 
         auto it2 = ni.crbegin();
-        REQUIRE(*it2 == 127);
-        REQUIRE(*(++it2) == 96);
-        REQUIRE(*(++it2) == 65);
-        REQUIRE(*(++it2) == 64);
-        REQUIRE(*(++it2) == 2);
-        REQUIRE(*(++it2) == 1);
-        REQUIRE(++it2 == ni.crend());
+        CHECK(*it2 == 127);
+        CHECK(*(++it2) == 96);
+        CHECK(*(++it2) == 65);
+        CHECK(*(++it2) == 64);
+        CHECK(*(++it2) == 2);
+        CHECK(*(++it2) == 1);
+        CHECK(++it2 == ni.crend());
     }
 
     SECTION("remove existing note from back") {
-        REQUIRE(ni.remove(127) == true);
-        REQUIRE(ni.size() == 6);
+        CHECK(ni.remove(127) == true);
+        CHECK(ni.size() == 6);
 
-        REQUIRE(ni.front() == 0);
-        REQUIRE(ni.back() == 96);
+        CHECK(ni.front() == 0);
+        CHECK(ni.back() == 96);
 
         auto it1 = ni.cbegin();
-        REQUIRE(*it1 == 0);
-        REQUIRE(*(++it1) == 1);
-        REQUIRE(*(++it1) == 2);
-        REQUIRE(*(++it1) == 64);
-        REQUIRE(*(++it1) == 65);
-        REQUIRE(*(++it1) == 96);
-        REQUIRE(++it1 == ni.cend());
+        CHECK(*it1 == 0);
+        CHECK(*(++it1) == 1);
+        CHECK(*(++it1) == 2);
+        CHECK(*(++it1) == 64);
+        CHECK(*(++it1) == 65);
+        CHECK(*(++it1) == 96);
+        CHECK(++it1 == ni.cend());
 
         auto it2 = ni.crbegin();
-        REQUIRE(*it2 == 96);
-        REQUIRE(*(++it2) == 65);
-        REQUIRE(*(++it2) == 64);
-        REQUIRE(*(++it2) == 2);
-        REQUIRE(*(++it2) == 1);
-        REQUIRE(*(++it2) == 0);
-        REQUIRE(++it2 == ni.crend());
+        CHECK(*it2 == 96);
+        CHECK(*(++it2) == 65);
+        CHECK(*(++it2) == 64);
+        CHECK(*(++it2) == 2);
+        CHECK(*(++it2) == 1);
+        CHECK(*(++it2) == 0);
+        CHECK(++it2 == ni.crend());
     }
 
     SECTION("remove all notes") {
-        REQUIRE(ni.remove(127) == true);
-        REQUIRE(ni.size() == 6);
-        REQUIRE(ni.front() == 0);
-        REQUIRE(ni.back() == 96);
-        REQUIRE(ni.remove(127) == false);
+        CHECK(ni.remove(127) == true);
+        CHECK(ni.size() == 6);
+        CHECK(ni.front() == 0);
+        CHECK(ni.back() == 96);
+        CHECK(ni.remove(127) == false);
 
-        REQUIRE(ni.remove(0) == true);
-        REQUIRE(ni.size() == 5);
-        REQUIRE(ni.front() == 1);
-        REQUIRE(ni.back() == 96);
-        REQUIRE(ni.remove(0) == false);
+        CHECK(ni.remove(0) == true);
+        CHECK(ni.size() == 5);
+        CHECK(ni.front() == 1);
+        CHECK(ni.back() == 96);
+        CHECK(ni.remove(0) == false);
 
-        REQUIRE(ni.remove(96) == true);
-        REQUIRE(ni.size() == 4);
-        REQUIRE(ni.front() == 1);
-        REQUIRE(ni.back() == 65);
-        REQUIRE(ni.remove(96) == false);
+        CHECK(ni.remove(96) == true);
+        CHECK(ni.size() == 4);
+        CHECK(ni.front() == 1);
+        CHECK(ni.back() == 65);
+        CHECK(ni.remove(96) == false);
 
-        REQUIRE(ni.remove(1) == true);
-        REQUIRE(ni.size() == 3);
-        REQUIRE(ni.front() == 2);
-        REQUIRE(ni.back() == 65);
-        REQUIRE(ni.remove(1) == false);
+        CHECK(ni.remove(1) == true);
+        CHECK(ni.size() == 3);
+        CHECK(ni.front() == 2);
+        CHECK(ni.back() == 65);
+        CHECK(ni.remove(1) == false);
 
-        REQUIRE(ni.remove(65) == true);
-        REQUIRE(ni.size() == 2);
-        REQUIRE(ni.front() == 2);
-        REQUIRE(ni.back() == 64);
-        REQUIRE(ni.remove(65) == false);
+        CHECK(ni.remove(65) == true);
+        CHECK(ni.size() == 2);
+        CHECK(ni.front() == 2);
+        CHECK(ni.back() == 64);
+        CHECK(ni.remove(65) == false);
 
-        REQUIRE(ni.remove(2) == true);
-        REQUIRE(ni.size() == 1);
-        REQUIRE(ni.front() == 64);
-        REQUIRE(ni.back() == 64);
-        REQUIRE(ni.remove(2) == false);
+        CHECK(ni.remove(2) == true);
+        CHECK(ni.size() == 1);
+        CHECK(ni.front() == 64);
+        CHECK(ni.back() == 64);
+        CHECK(ni.remove(2) == false);
 
-        REQUIRE(ni.remove(64) == true);
-        REQUIRE(ni.size() == 0);
-        REQUIRE(ni.front() == NO_NOTE);
-        REQUIRE(ni.back() == NO_NOTE);
-        REQUIRE(ni.cbegin() == ni.cend());
-        REQUIRE(ni.crbegin() == ni.crend());
-        REQUIRE(ni.remove(64) == false);
+        CHECK(ni.remove(64) == true);
+        CHECK(ni.size() == 0);
+        CHECK(ni.front() == NO_NOTE);
+        CHECK(ni.back() == NO_NOTE);
+        CHECK(ni.cbegin() == ni.cend());
+        CHECK(ni.crbegin() == ni.crend());
+        CHECK(ni.remove(64) == false);
     }
 }
 
-TEST_CASE("insert every possible note", TEST_GROUP) {
+TEST_CASE("insert every possible note", NOTE_INDEX_TAG) {
     NoteIndex ni;
     for (NoteNumber n = 0; isValidNote(n); n++) {
         ni.insert(n);
     }
-    REQUIRE(ni.size() == 128);
-    REQUIRE(ni.front() == 0);
-    REQUIRE(ni.back() == 127);
+    CHECK(ni.size() == 128);
+    CHECK(ni.front() == 0);
+    CHECK(ni.back() == 127);
 }
 
-TEST_CASE("clear", TEST_GROUP) {
+TEST_CASE("clear", NOTE_INDEX_TAG) {
     NoteIndex ni;
     for (NoteNumber n = 0; isValidNote(n); n++) {
         ni.insert(n);
     }
     ni.clear();
 
-    REQUIRE(ni.size() == 0);
-    REQUIRE(ni.front() == NO_NOTE);
-    REQUIRE(ni.back() == NO_NOTE);
-    REQUIRE(ni.cbegin() == ni.cend());
-    REQUIRE(ni.crbegin() == ni.crend());
+    CHECK(ni.size() == 0);
+    CHECK(ni.front() == NO_NOTE);
+    CHECK(ni.back() == NO_NOTE);
+    CHECK(ni.cbegin() == ni.cend());
+    CHECK(ni.crbegin() == ni.crend());
 }
