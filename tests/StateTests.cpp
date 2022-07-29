43b9866ba5402a48731869488c5fbac772983233
Kevin Schuetz <schuetzk@gmail.com>
2022-07-28 23:50:10 -0500

    More tests for State

--- tests/StateTests.cpp
+++ tests/StateTests.cpp
@@ -91,3 +91,103 @@ TEST_CASE("activateNote", STATE_TAG) {
         }
     }
 }
+
+TEST_CASE("deactivateNote", STATE_TAG) {
+    State state;
+
+    state.activateNote(0, MaxVelocity, Timestamp(1));
+    state.activateNote(1, MaxVelocity, Timestamp(1));
+    state.activateNote(2, MaxVelocity, Timestamp(1));
+    state.activateNote(64, MaxVelocity, Timestamp(1));
+    state.activateNote(65, MaxVelocity, Timestamp(1));
+    state.activateNote(96, MaxVelocity, Timestamp(1));
+    state.activateNote(127, MaxVelocity, Timestamp(1));
+
+    SECTION("deactivate note that isn't active") {
+        state.deactivateNote(10);
+        CHECK(state.playingCount() == 7);
+        CHECK(state.noteStatus(0) == NoteStatus::Playing);
+        CHECK(state.noteStatus(1) == NoteStatus::Playing);
+        CHECK(state.noteStatus(2) == NoteStatus::Playing);
+        CHECK(state.noteStatus(64) == NoteStatus::Playing);
+        CHECK(state.noteStatus(65) == NoteStatus::Playing);
+        CHECK(state.noteStatus(96) == NoteStatus::Playing);
+        CHECK(state.noteStatus(127) == NoteStatus::Playing);
+        CHECK_THAT(state.noteIndex(), IteratesForward({0, 1, 2, 64, 65, 96, 127}));
+    }
+
+    SECTION("deactivate note in front") {
+        state.deactivateNote(0);
+        CHECK(state.playingCount() == 6);
+        CHECK(state.noteStatus(0) == NoteStatus::NotPlaying);
+        CHECK(state.noteStatus(1) == NoteStatus::Playing);
+        CHECK(state.noteStatus(2) == NoteStatus::Playing);
+        CHECK(state.noteStatus(64) == NoteStatus::Playing);
+        CHECK(state.noteStatus(65) == NoteStatus::Playing);
+        CHECK(state.noteStatus(96) == NoteStatus::Playing);
+        CHECK(state.noteStatus(127) == NoteStatus::Playing);
+        CHECK_THAT(state.noteIndex(), IteratesForward({1, 2, 64, 65, 96, 127}));
+    }
+
+    SECTION("deactivate note in middle") {
+        state.deactivateNote(64);
+        CHECK(state.playingCount() == 6);
+        CHECK(state.noteStatus(0) == NoteStatus::Playing);
+        CHECK(state.noteStatus(1) == NoteStatus::Playing);
+        CHECK(state.noteStatus(2) == NoteStatus::Playing);
+        CHECK(state.noteStatus(64) == NoteStatus::NotPlaying);
+        CHECK(state.noteStatus(65) == NoteStatus::Playing);
+        CHECK(state.noteStatus(96) == NoteStatus::Playing);
+        CHECK(state.noteStatus(127) == NoteStatus::Playing);
+        CHECK_THAT(state.noteIndex(), IteratesForward({0, 1, 2, 65, 96, 127}));
+    }
+
+    SECTION("deactivate note in back") {
+        state.deactivateNote(127);
+        CHECK(state.playingCount() == 6);
+        CHECK(state.noteStatus(0) == NoteStatus::Playing);
+        CHECK(state.noteStatus(1) == NoteStatus::Playing);
+        CHECK(state.noteStatus(2) == NoteStatus::Playing);
+        CHECK(state.noteStatus(64) == NoteStatus::Playing);
+        CHECK(state.noteStatus(65) == NoteStatus::Playing);
+        CHECK(state.noteStatus(96) == NoteStatus::Playing);
+        CHECK(state.noteStatus(127) == NoteStatus::NotPlaying);
+        CHECK_THAT(state.noteIndex(), IteratesForward({0, 1, 2, 64, 65, 96}));
+    }
+
+    SECTION("deactivate multiple notes") {
+        state.deactivateNote(1);
+        state.deactivateNote(64);
+        state.deactivateNote(96);
+        CHECK(state.playingCount() == 4);
+        CHECK(state.noteStatus(0) == NoteStatus::Playing);
+        CHECK(state.noteStatus(1) == NoteStatus::NotPlaying);
+        CHECK(state.noteStatus(2) == NoteStatus::Playing);
+        CHECK(state.noteStatus(64) == NoteStatus::NotPlaying);
+        CHECK(state.noteStatus(65) == NoteStatus::Playing);
+        CHECK(state.noteStatus(96) == NoteStatus::NotPlaying);
+        CHECK(state.noteStatus(127) == NoteStatus::Playing);
+        CHECK_THAT(state.noteIndex(), IteratesForward({0, 2, 65, 127}));
+    }
+
+}
+
+TEST_CASE("clear", STATE_TAG) {
+    State state;
+
+    state.activateNote(0, MaxVelocity, Timestamp(1));
+    state.activateNote(1, MaxVelocity, Timestamp(1));
+    state.activateNote(2, MaxVelocity, Timestamp(1));
+    state.activateNote(64, MaxVelocity, Timestamp(1));
+    state.activateNote(65, MaxVelocity, Timestamp(1));
+    state.activateNote(96, MaxVelocity, Timestamp(1));
+    state.activateNote(127, MaxVelocity, Timestamp(1));
+
+    state.clear();
+
+    CHECK(state.playingCount() == 0);
+    for (NoteNumber n = 0; isValidNote(n); n++) {
+        CHECK(state.noteStatus(n) == NoteStatus::NotPlaying);
+    }
+    CHECK_THAT(state.noteIndex(), IteratesForward({}));
+}
